b9aa5cfae2bc70e7f1e401cfa18635ab5a80da8c
szederd <szeder.divad@gmail.com>
2023-07-30 17:44:36 +0200

    update

--- tictactoe-frontend/src/environments/environment.development.ts
+++ tictactoe-frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    BASE_URL: 'https://localhost:5013/game/',
-    SIGNALR_URL: 'https://localhost:5013/game-live/'
+    BASE_URL: 'http://localhost:5013/game/',
+    SIGNALR_URL: 'http://localhost:5013/game-live/'
 };
